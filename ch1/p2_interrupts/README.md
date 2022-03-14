af49c57b90ecae8d0f3cf59932f801edca19c48e
Ankit Chandawala <ankitchandawala@gmail.com>
2022-03-14 00:28:44 +0000

    Add simple interrupt to display on screen

---
+++ ch1/p2_interrupts/README.md
@@ -0,0 +1,4 @@
+# Interrupts
+
+We will now write a simple interrupt which will display a message on the screen. Take a look at the `simple_interrupt.asm` file to see how the interrupt is triggered and a message displayed on the screen.
+
---
+++ ch1/p2_interrupts/simple_interrupt.asm
@@ -0,0 +1,23 @@
+; Invoke BIOS teletype routine
+mov ah, 0x0e
+
+; Store the character to the printed on the screen in the register
+mov al, 'H'
+; Trigger instruction
+int 0x10
+mov al, 'e'
+int 0x10
+mov al, 'l'
+int 0x10
+mov al, 'l'
+int 0x10
+mov al, 'o'
+int 0x10
+
+; Padding to make 512 bytes of memory
+times 510-($-$$) db 0
+
+; Boot identifier
+dw 0xaa55
+
+
